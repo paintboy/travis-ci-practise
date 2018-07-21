06ced59a4bd2416db58da24dd0bea4d59a14a89e
Paint Boy <37026467+paintboy@users.noreply.github.com>
2018-07-21 18:03:47 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 node crawl.js
-printf "\n\nCrawled Pages:\n$(cat crawled.csv)\n\nContent Security Policy Reports:\n$(cat csp-reports.json)"
+printf "\nCrawled Pages:\n$(cat crawled.csv)\n\nContent Security Policy Reports:\n$(cat csp-reports.json)"
