7683dd51f46df9d4f42824e594d87d07a44d3ec2
Paint Boy <37026467+paintboy@users.noreply.github.com>
2018-07-21 03:29:00 +0100

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -1,10 +1,26 @@
 const HCCrawler = require('headless-chrome-crawler');
 
+/*class Crawler {
+  async _setBypassCSP() {
+    //if (!this._options.jQuery) return;
+    // @ts-ignore
+    await this._page.setBypassCSP(false);
+  }
+}*/
+
 (async () => {
   const crawler = await HCCrawler.launch({
     maxDepth: 1,
+    jQuery: false
   });
-  await crawler.queue('http://localhost:8080');
+  /*class Crawler {
+    async _setBypassCSP() {
+      //if (!this._options.jQuery) return;
+      // @ts-ignore
+      await this._page.setBypassCSP(false);
+    }
+  }*/
+  await crawler.queue({ url: 'http://localhost:8080/', saveAs: 'localhost.png' });
   await crawler.onIdle();
   console.log("Hello");
   await crawler.close();
