ef2f31933ef1107243d22519b08424ebf4895a62
Paint Boy <37026467+paintboy@users.noreply.github.com>
2018-07-22 14:43:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 node crawl.js
-printf "\nCrawled Pages:\n$(cat crawled.csv)\n\nContent Security Policy Reports:\n$(cat csp-reports.json)"
+printf "\nCrawled Pages:\n$(cat crawled.csv)\n\n"
+if [ -s csp-reports.json ]; then printf "Content Security Policy Reports:\n$(cat csp-reports.json)"; else printf "No Reports Generated! Success!"; fi
