66aeaa499fce74dcdacca7b550b6de1b41b6b288
Paint Boy <37026467+paintboy@users.noreply.github.com>
2018-07-29 12:42:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "${TRAVIS_BUILD_NUMBER}"
 node crawl.js
 printf "\nCrawled Pages:\n$(cat crawled.csv)\n\n"
 if [ -s csp-reports.txt ]; then printf "Content Security Policy Reports:\n$(cat csp-reports.txt)"; else printf "No Reports Generated! Success!"; fi
